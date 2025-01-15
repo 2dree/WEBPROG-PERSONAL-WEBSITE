63a3609e9ea890c9c460c79a56ac133112e26cb7
2dree <pjco3@student.apc.edu.ph>
2025-01-15 03:28:52 -0800

    updated

--- home/js/gallery-vue.js
+++ home/js/gallery-vue.js
@@ -1,29 +1,36 @@
 // Initialize Vue App
 const app = Vue.createApp({
-    data() {
-      return {
-        galleryImages: [
-          {
-            url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9EmUMBVgD0pedbBOC4snbso31LX0ww1ylxg&s",
-            alt: "Gallery Image 1",
-            description: "Image 1 Description",
-          },
-          {
-            url: "https://waltgracevintage.com/cdn/shop/files/IMG_0372.jpg?v=1722625579",
-            alt: "Gallery Image 2",
-            description: "Image 2 Description",
-          },
-          // Add more image objects as needed
-          {
-            url: "https://live.staticflickr.com/667/21209906942_a9cc7f3d9b_b.jpg",
-            alt: "Placeholder Image",
-            description: "Image 3 Description",
-          },
-        ],
-      };
-    },
-  });
+  data() {
+    return {
+      galleryImages: [
+        {
+          url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9EmUMBVgD0pedbBOC4snbso31LX0ww1ylxg&s",
+          alt: "Gallery Image 1",
+          description: "Image 1 Description",
+        },
+        {
+          url: "https://waltgracevintage.com/cdn/shop/files/IMG_0372.jpg?v=1722625579",
+          alt: "Gallery Image 2",
+          description: "Image 2 Description",
+        },
+        // Add more image objects as needed
+        {
+          url: "https://live.staticflickr.com/667/21209906942_a9cc7f3d9b_b.jpg",
+          alt: "Placeholder Image",
+          description: "Image 3 Description",
+        },
+      ],
+    };
+  },
+});
+
+
+    function goToHome() {
+      window.location.href = "index.html";
+    }
   
-  // Mount Vue App
-  app.mount("#gallery-app");
-  
+
+// Mount Vue App
+app.mount("#gallery-app");
+
+
